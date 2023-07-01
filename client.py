3726838110097d1ec42f8c7c7fcbcac794f31677
Bjorn Talastas <talastasbjorn@gmail.com>
2023-07-01 18:15:58 -0400

    created functions for user preferences prompts

--- client.py
+++ client.py
@@ -17,18 +17,7 @@ FILE_FORMAT = {1: "CSV", 2: "JSON"}
 SPORTS = {1: "NBA", 2: "MLB", 3: "NFL", 4: "NHL"}
 
 
-def main():
-    args = sys.argv[1:]
-    if len(args) != 2:
-        sys.stderr.write("Error running program. ./client.py <port-#> <IP-Address>")
-    print(f"{LINE_BREAKER}\n\n{WELCOME_MESSAGE}\n\n{LINE_BREAKER}\n\n")
-
-    # args[0] -> port number; args[1] -> server ip address
-    server_address = (int(args[0]), args[1])
-
-    # ask user for preferences
-    # sports type and save file type
-
+def sports_prompt():
     while True:
         print("Which sports are you looking for?\n1. NBA\t2. MLB\n3. NFL\tNHL\n")
 
@@ -46,6 +35,8 @@ def main():
         else:
             print("Enter valid number.")
 
+
+def file_prompt():
     while True:
         print("What file format do you want the data to be saved in?\n1. JSON\t2. CSV")
         file_format = input()
@@ -59,6 +50,21 @@ def main():
             break
         else:
             print("Enter valid number.")
+
+
+def main():
+    args = sys.argv[1:]
+    if len(args) != 2:
+        sys.stderr.write("Error running program. ./client.py <port-#> <IP-Address>")
+    print(f"{LINE_BREAKER}\n\n{WELCOME_MESSAGE}\n\n{LINE_BREAKER}\n\n")
+
+    # args[0] -> port number; args[1] -> server ip address
+    server_address = (int(args[0]), args[1])
+
+    # ask user for preferences
+    # sports type and save file type
+    sports_prompt()
+    file_prompt()
 
 
 if __name__ == "__main__":
