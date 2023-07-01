ec40c1768eed1df247123351bd9e75ab0d9912e5
Bjorn Talastas <talastasbjorn@gmail.com>
2023-07-01 17:27:37 -0400

    command line arguments checking

--- client.py
+++ client.py
@@ -1,10 +1,25 @@
+#!/usr/bin/env python3
+import sys
 import threading
 import socket
 
 # need socket, data processing thread function.
 
+
 def main():
-    pass
+    args = sys.argv[1:]
+    if len(args) != 2:
+        sys.stderr.write("Error running program. ./client.py <port-#> <IP-Address>")
+
+    # args[0] -> port number; args[1] -> server ip address
+    server_address = (int(args[0]), args[1])
+    print(f"server address: {server_address}")
+
+    # ask user for preferences
+    # sports type and save file type
+
+    print("Which sports are you looking for?" "1. NBA\t2. MLB\n3. NFL\tNHL\n")
+
 
 if __name__ == "__main__":
     main()
