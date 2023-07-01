2e524e2d6e605b0556e41e6e74343b9c0a9fc9a8
Bjorn Talastas <talastasbjorn@gmail.com>
2023-07-01 17:52:31 -0400

    prompt for sports category

--- client.py
+++ client.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
+from ast import IsNot
+import numbers
 import sys
 import threading
 import socket
+
+
+from zmq import NULL
 
 # need socket, data processing thread function.
 
 WELCOME_MESSAGE = "Welcome to btal sports and stats"
 LINE_BREAKER = "-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-"
+
+FILE_FORMAT = {1: "CSV", 2: "JSON"}
+SPORTS = {1: "NBA", 2: "MLB", 3: "NFL", 4: "NHL"}
 
 
 def main():
@@ -21,10 +29,28 @@ def main():
     # ask user for preferences
     # sports type and save file type
 
-    print("Which sports are you looking for?\n1. NBA\t2. MLB\n3. NFL\tNHL\n")
+    while True:
+        print("Which sports are you looking for?\n1. NBA\t2. MLB\n3. NFL\tNHL\n")
 
-    sports_category = input()
-    print(sports_category)
+        sports_category = input()
+        if int(sports_category) < 0 or int(sports_category) > 4:
+            print("Enter valid number.")
+            pass
+        sport = SPORTS.get(int(sports_category))
+        if sport is not None:
+            break
+        else:
+            print("Enter valid number.")
+
+    while True:
+        print("What file format do you want the data to be saved in?\n1. JSON\t2. CSV")
+        file_format = int(input())
+
+        if file_format < 0 or file_format > 2:
+            print("Enter valid number.")
+            pass
+
+    print(sport)
 
 
 if __name__ == "__main__":
