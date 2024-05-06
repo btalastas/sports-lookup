e3325b7c07035912a588303e6df79cb59a94d65b
Bjorn Talastas <talastasbjorn@gmail.com>
2024-05-06 13:05:42 -0400

    function to extract pitchers from mlb games

---
+++ js/extractPitcherNames.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {Object} schedule mlb Schedule for current day.
+ * @returns {Array} list of objects for home and away pitchers.
+ */
+const extractPitchers = (schedule) => {
+  let pitchers = [];
+  schedule.map((games) => {
+    let currentPitchers = {
+      home: games["home_probable_pitcher"],
+      away: games["away_probable_pitcher"],
+    };
+    pitchers.push(currentPitchers);
+  });
+  return pitchers;
+};
