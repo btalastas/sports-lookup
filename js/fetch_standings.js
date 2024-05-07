add4a960c1579847f30fb9d556316850b3a78fd8
btal <talastasbjorn@gmail.com>
2024-05-07 14:25:04 -0400

    fetch standings for mlb

---
+++ js/fetch_standings.js
@@ -0,0 +1,25 @@
+import fetch from "node-fetch";
+
+// const url =
+//   "https://yl4lgoitba.execute-api.us-east-1.amazonaws.com/prod/mlb_standings";
+
+/**
+ *
+ * @param {String} url AWS API url
+ * @returns {Object} object for standings for each division
+ */
+const fetchStandings = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.body;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+};
+
+export default fetchStandings;
