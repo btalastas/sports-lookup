24fb1b423c3c0060720401ebd2bc6ba9a5a581a0
btal <talastasbjorn@gmail.com>
2024-05-13 14:10:06 -0400

    changed Schedule component to take in a prop for mlb games

--- front-end/sports-lookup/src/components/Schedule.js
+++ front-end/sports-lookup/src/components/Schedule.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import {
   Typography,
   Card,
@@ -6,41 +5,9 @@ import {
   List,
   ListItem,
   ListItemText,
-  CircularProgress,
 } from "@mui/material";
 
-import fetchSchedule from "../utils/fetch_schedule.js";
-import extractPitchers from "../utils/extractPitcherNames.js";
-
-const url =
-  "https://yl4lgoitba.execute-api.us-east-1.amazonaws.com/prod/mlb_schedule";
-
-export default function Schedule() {
-  const [games, setGames] = useState([]);
-  const [pitchers, setPitchers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const loadSchedule = async () => {
-      const scheduleData = await fetchSchedule(url);
-      setGames(scheduleData);
-      setLoading(false);
-      setPitchers(extractPitchers(games));
-    };
-
-    loadSchedule();
-  }, []);
-
-  if (loading) {
-    return (
-      <div
-        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
-      >
-        <CircularProgress />
-      </div>
-    );
-  }
-
+export default function Schedule({ games }) {
   return (
     <Card raised style={{ margin: "20px", backgroundColor: "#fafafa" }}>
       <CardContent>
