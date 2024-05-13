21e92812d04853ef1048469eba23696499ff904e
btal <talastasbjorn@gmail.com>
2024-05-13 14:11:12 -0400

    fetched schedule and standings

--- front-end/sports-lookup/src/App.js
+++ front-end/sports-lookup/src/App.js
@@ -1,56 +1,77 @@
 import { useEffect, useRef, useState } from "react";
 import HeaderNav from "./header-nav.jsx";
-import { Container } from "@mui/material";
+import { Container, CircularProgress } from "@mui/material";
 import fetch from "node-fetch";
 
+import fetchSchedule from "./utils/fetch_schedule.js";
+import fetchStandings from "./utils/fetch_standings.js";
+import Schedule from "./components/Schedule.js";
+import Standings from "./components/Standings.js";
 
 function App() {
+  // MLB Schedule
+  const [games, setGames] = useState([]);
 
-  const [res, setRes] = useState([]);
-  const hasFetchedData = useRef(false);
+  // MLB Standings
+  const [standings, setStandings] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // const [res, setRes] = useState([]);
+  // const hasFetchedData = useRef(false);
   // const hasFetchedStandings = useRef(false);
 
-  const url =
-    "https://yl4lgoitba.execute-api.us-east-1.amazonaws.com/prod/mlb_schedule";
-
-  // const standingsURL =
-  //   "https://yl4lgoitba.execute-api.us-east-1.amazonaws.com/prod/mlb_standings";
-
-  const fetchData = (() => {
-      fetch(url)
-      .then((response) => response.json()) // Parsing the JSON response
-      .then((data) => setRes(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  })
+  // const fetchData = () => {
+  //   fetch(url)
+  //     .then((response) => response.json()) // Parsing the JSON response
+  //     .then((data) => setRes(data))
+  //     .catch((error) => console.error("Error fetching data:", error));
+  // };
   useEffect(() => {
+    const scheduleData = fetchSchedule(
+      process.env.REACT_APP_MLB_SCHEDULE_API_URL
+    );
 
-    if (hasFetchedData.current === false) {
-      fetchData();
-      hasFetchedData.current = true;
-    } 
+    const standingsData = fetchStandings(
+      process.env.REACT_APP_MLB_STANDINGS_API_URL
+    );
+
+    Promise.all([scheduleData, standingsData])
+      .then(([response1, response2]) => {
+        setGames(response1);
+        setStandings(response2);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch data:", error);
+        setError("Failed to fetch data");
+      });
   }, []);
 
+  if (loading) {
+    return (
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  }
 
-  // const fetchStandings = (() => {
-  //   fetch(standingsURL, {
-  //     mode: "cors",
-  //     method: "GET",
-  //     headers: { "Content-Type": "application/json" },
-  //   })
-  //     .then((response) => response.json()) // Parsing the JSON response
-  //     .then((data) => console.log(data))
-  //     .catch((error) => console.error("Error fetching data:", error));
-  // })
-
-  
-
-
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+  console.log(standings);
   return (
-    <Container sx={{ height: "100vh" }}>
-      <Container sx={{padding: 0, width: "300vh"}}>
-        {/* <HeaderNav res={res}/> */}
-      </Container>
-    </Container>
+    <Schedule games={games} />
+    // <Standings />
+    // <Container sx={{ height: "100vh" }}>
+
+    //   <Container sx={{ padding: 0, width: "300vh" }}>
+    //     {/* <HeaderNav res={res}/> */}
+    //   </Container>
+    // </Container>
   );
 }
 
