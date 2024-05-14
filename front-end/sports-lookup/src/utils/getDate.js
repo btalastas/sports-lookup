3dac7ea672ebab81d128dbaf577303299a1ceaa4
btal <talastasbjorn@gmail.com>
2024-05-14 14:53:53 -0400

    created function to get current date

---
+++ front-end/sports-lookup/src/utils/getDate.js
@@ -0,0 +1,9 @@
+export const getDate = () => {
+  const date = new Date();
+  const dayOfWeek = date.toLocaleString("en-US", { weekday: "long" }); // "Friday"
+  const day = date.getDate().toString().padStart(2, "0"); // "31"
+  const month = date.toLocaleString("en-US", { month: "long" }); // "March"
+  const year = date.getFullYear(); // 2023
+
+  return `${dayOfWeek}, ${day} ${month} ${year}`; // "Friday, 31 March 2023"
+};
