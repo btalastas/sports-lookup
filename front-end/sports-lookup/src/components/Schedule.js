fe2392b234a8ac2420f72b1c46568e590279c1c9
btal <talastasbjorn@gmail.com>
2024-05-14 14:54:32 -0400

    displayed current date and venue

--- front-end/sports-lookup/src/components/Schedule.js
+++ front-end/sports-lookup/src/components/Schedule.js
@@ -7,19 +7,32 @@ import {
   ListItemText,
 } from "@mui/material";
 
+import { getDate } from "../utils/getDate.js";
+
 export default function Schedule({ games }) {
   return (
     <Card raised style={{ margin: "20px", backgroundColor: "#fafafa" }}>
       <CardContent>
-        <Typography variant="h4" gutterBottom>
-          MLB Game Schedule
+        <Typography variant="h4" gutterBottom align="center">
+          {`MLB Game Schedule for ${getDate()}`}
         </Typography>
         {games.length > 0 ? (
           <List>
             {games.map((game, index) => (
               <ListItem key={index} divider>
                 <ListItemText
-                  primary={`${game.away_name} @ ${game.home_name}`}
+                  align="center"
+                  primary={
+                    <>
+                      {`${game.away_name} @ ${game.home_name}`}
+                      <br />
+                      {`${game.game_date} | ${game.venue_name}`}
+                      <br />
+                      {game.doubleheader === "Y"
+                        ? `Doubleheader game: ${game.game_num}`
+                        : null}
+                    </>
+                  }
                   secondary={`${game.away_probable_pitcher} vs ${game.home_probable_pitcher}`}
                 />
               </ListItem>
